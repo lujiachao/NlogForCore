208f9615ac1396f86a206362c8affc7bcfcc0865
lujiachao <632849093@qq.com>
2019-03-20 15:22:27 +0800

    添加项目文件。

---
+++ BackgroundHost/TokenRefreshService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FL.BackgroundHost
+{
+    internal class TokenRefreshService : BackgroundService
+    {
+        private readonly ILogger _logger;
+
+        public TokenRefreshService(ILogger<TokenRefreshService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Service starting");
+
+            _logger.LogError("DASAD ");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogTrace(DateTime.Now.ToLongTimeString() + ": Trace ");
+                _logger.LogDebug(DateTime.Now.ToLongTimeString() + ": Debug ");
+                _logger.LogError(DateTime.Now.ToLongTimeString() + ": Error ");
+                _logger.LogWarning(DateTime.Now.ToLongTimeString() + ": Warning ");
+                _logger.LogCritical(DateTime.Now.ToLongTimeString() + ": Critical ");
+                _logger.LogInformation(DateTime.Now.ToLongTimeString() + ": Information");
+                await Task.Delay(15000, stoppingToken);
+            }
+
+            _logger.LogInformation("Service stopping");
+        }
+    }
+}
